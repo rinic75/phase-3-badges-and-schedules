67c42b3ce70ce52f1584dce6757319e36bf61394
Chang Shin <cshin7374@gmail.com>
2022-09-23 01:10:45 -0500

    commit tree 9478802cc01971159474162b6531149058b26b06

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
+require 'pry'
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  names.map do |name|
+    badge_maker(name)
+  end
+  
+end
+
+def assign_rooms(names)
+  
+  names.map.with_index do |name, index|
+    "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  end
+end
+
+def printer(names)
+  batch_badge_creator(names).each do |badge|
+    puts badge
+  end
+  assign_rooms(names).each do |room|
+    puts room
+  end
+end
